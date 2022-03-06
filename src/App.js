ba58fb5d6e5a644c773553852efb0e42b59396cc
goeun208 <qop341@gmail.com>
2022-03-06 15:11:06 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Problems from './components/Problems';
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <BrowserRouter>
+    <Switch>
+      {/* <Route path={"/home"} exact component={Home} /> */}
+      <Route path={"/"} exact>
+        <Problems />
+      </Route>
+      <Route path={"/problems"} exact>
+        <Problems />
+      </Route>
+    </Switch>
+    </BrowserRouter>
   );
 }
 

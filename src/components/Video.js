b637dabd79c5a7cf9dd2e0d65ad66446b91a4a50
chaeli <s.kcl5243@gmail.com>
2022-03-27 18:09:01 +0900

    03/27

--- src/components/Video.js
+++ src/components/Video.js
@@ -8,8 +8,10 @@ import inf from "./image/in.png"
 import back from "./image/back.png"
 import forward from "./image/forward.png"
 
-let jsvideo = [[jscode, "https://youtu.be/PZIPsKgWJiw"],[jscode, "https://youtu.be/PZIPsKgWJiw"], [jscode, "https://youtu.be/PZIPsKgWJiw"]];
+
+let jsvideo = [[jscode, "https://youtu.be/PZIPsKgWJiw", 1],[jscode, "https://youtu.be/PZIPsKgWJiw", 2], [jscode, "https://youtu.be/PZIPsKgWJiw", 3]];
 let rvideo = [[nomad, "https://youtu.be/YNEdj1MNbok"],[rtodo, "https://youtu.be/51zgTcx3_9w"],[inf, "https://youtu.be/fgoMqmNKE18"]];
+
 
 function Video() {
     return(
@@ -17,32 +19,40 @@ function Video() {
             <div className={styles.head}>GDSC Frotend study</div>
             <div className={styles.jsblock}>
                 <h2><MdCircle color="#6B6FAE" size={25}/> JavaScript</h2>
-                <div className={styles.imgbox}>
-                    <img src={back}/>
-                    {jsvideo.map((item) =>
-                    (
-                        <div>
-                            <a href={item[1]} target="_blank">
-                                <img src={item[0]} />
-                            </a>
-                        </div>
-                    ))}
-                    <img src={forward}/>
+                <div className={styles.imgs}>
+                    {/*<button><MdArrowBackIos size={70} /> </button>*/}
+                    <img src={back} className={styles.icon}/>
+                    <div className={styles.imgbox}>
+                        {jsvideo.map((jsv) =>
+                        (
+                            <div key={jsv[2]}>
+                                <a href={jsv[1]} target="_blank" rel="noopener noreferrer">
+                                    <img src={jsv[0]} className={styles.jsimg} />
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                    <img src={forward} className={styles.icon}/>
                 </div>
+                {/*<button><MdArrowForwardIos size={70} /> </button>*/}
             </div>
             <div className={styles.rblock}>
-                <h2><MdCircle color="#6B6FAE" size={25}/> ReactJS</h2>
-                <div className={styles.imgbox}>
-                <button><MdArrowBackIos size={70} /> </button>
-                    {rvideo.map((item) =>
-                    (
-                        <div>
-                            <a href={item[1]} target="_blank">
-                                <img src={item[0]} />
-                            </a>
-                        </div>
-                    ))}
-                    <button><MdArrowForwardIos size={70} /> </button>
+                <h2><MdCircle color="#6B6FAE" size={25} /> ReactJS</h2>
+                <div className={styles.imgs}>
+                    <img src={back} className={styles.icon}/>
+                    <div className={styles.imgbox}>
+                        {/*<button><MdArrowBackIos size={70} /> </button>*/}
+                        {rvideo.map((rv) =>
+                        (
+                            <div key={rv}>
+                                <a href={rv[1]} target="_blank" rel="noopener noreferrer">
+                                    <img src={rv[0]} className={styles.jsimg} />
+                                </a>
+                            </div>
+                        ))}
+                        {/*<button><MdArrowForwardIos size={70} /> </button>*/}
+                    </div>
+                    <img src={forward} className={styles.icon}/>
                 </div>
             </div>
         </div>
